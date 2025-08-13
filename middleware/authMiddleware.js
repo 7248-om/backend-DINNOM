42a254959157c1e7d039b5a285fd532104bf4dc0
Om Joshi <joshiom564@gmail.com>
2025-08-13 23:05:00 +0530

    bug fix

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.js';
+import User from '../models/User.js';
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your-super-secret-key-that-is-long-and-secure';
 
